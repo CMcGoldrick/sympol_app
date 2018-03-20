04e052c82929dc01cc516b3234406b0d739eae0e
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-03-19 19:05:49 -0500

    fixed error #2 in app/config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
       storage: :s3,
       s3_host_name: 's3-us-east-2.amazonaws.com',
       s3_credentials: {
-        bucket: ENV.fetch['S3_BUCKET_NAME'],
-        access_key_id: ENV.fetch['AWS_ACCESS_KEY_ID'],
-        secret_access_key: ENV.fetch['AWS_SECRET_ACCESS_KEY'],
-        s3_region: ENV.fetch['AWS_REGION'],
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        s3_region: ENV.fetch('AWS_REGION'),
       }
   }
   # Settings specified here will take precedence over those in config/application.rb.
