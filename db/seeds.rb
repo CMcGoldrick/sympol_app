db5ec76550f38e00ef2e33cacd00d7b9fed77b29
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-03-19 19:49:47 -0500

    created initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+        # PRODUCTION
 # Sympol.create!([
 #     {name: "Triquetra", origin: "Celtic", description: "Latin meaning 'threecornered' It's a holy symbol, and it's meanings are many. It varies in its aspects of spirit, nature, being-ness, and of the cosmos. This trinity symbol, as most others, reflects core concepts such as: Mind, Body, Spirit. Afterlife, Underworld, Physical life. Tribe, Country, Deity (god/goddess). These are just a few trinities folded within this Celtic symbol meaning. There are many more. The differences lay in the intent of the symbol, and the tribe that was using at the time in Celtic history. The negative space in the center of this symbol symbolizes the center of all influences. In personal ritual, it could represent the self at the center of trinity influences. In a tribe, the center is the heart of the people with the three elements creating an unbreakable union."},
 #     {name: "Triskele", origin: "Celtic", description: "This is another trinity symbol often found among ancient Celtic relics. It is sometimes referred to as a triskelion. The terms are often considered synonymous. This Celtic symbol meaning deals with the drawing of the three powers of maiden, mother and crone. It is often considered a sign of female power and especially power through transition and growth. This, however, is not an exclusive definition. Sometimes the symbol represents religious aspects of Father, Son, Holy Ghost. This representation came much later amongst Celtic descendants who were exposed to Christian conversion. In any instance, this trinity symbol can represent a triangle of foundational necessity. Meaning - you cannot have a healthy whole without all three elements joined together. Hence: Mind, Body, Spirit. Sun, Moon, Earth. Light, Dark, Gloaming (twilight). Not one energy can exist without the others."},
@@ -157,40 +158,87 @@
 # ])
 
 # SympolTrait.create! ([
-    # {sympol_id: 3, trait_id: 12},
-    # {sympol_id: 3, trait_id: 19},
-    # {sympol_id: 3, trait_id: 7},
+#     {sympol_id: 3, trait_id: 12},
+#     {sympol_id: 3, trait_id: 19},
+#     {sympol_id: 3, trait_id: 7},
 
-    # {sympol_id: 1, trait_id: 19},
+#     {sympol_id: 1, trait_id: 19},
 
-    # {sympol_id: 4, trait_id: 4},
-    # {sympol_id: 4, trait_id: 9},
-    # {sympol_id: 4, trait_id: 19},
-    # {sympol_id: 4, trait_id: 9},
-    # {sympol_id: 4, trait_id: 7},
+#     {sympol_id: 4, trait_id: 4},
+#     {sympol_id: 4, trait_id: 9},
+#     {sympol_id: 4, trait_id: 19},
+#     {sympol_id: 4, trait_id: 9},
+#     {sympol_id: 4, trait_id: 7},
     
-    # {sympol_id: 5, trait_id: 7},
-    # {sympol_id: 5, trait_id: 6},
-    # {sympol_id: 5, trait_id: 4},
-    # {sympol_id: 5, trait_id: 2},
+#     {sympol_id: 5, trait_id: 7},
+#     {sympol_id: 5, trait_id: 6},
+#     {sympol_id: 5, trait_id: 4},
+#     {sympol_id: 5, trait_id: 2},
 
-    # {sympol_id: 21, trait_id: 8},
-    # {sympol_id: 21, trait_id: 5},
-    # {sympol_id: 21, trait_id: 4},
-    # {sympol_id: 21, trait_id: 2},
-    # {sympol_id: 21, trait_id: 17}
-  # ])
+#     {sympol_id: 21, trait_id: 8},
+#     {sympol_id: 21, trait_id: 5},
+#     {sympol_id: 21, trait_id: 4},
+#     {sympol_id: 21, trait_id: 2},
+#     {sympol_id: 21, trait_id: 17}
+#   ])
 
   
+      # HEROKU
+
+Sympol.create!([
+    {name: "Triquetra", origin: "Celtic", description: "Latin meaning 'threecornered' It's a holy symbol, and it's meanings are many. It varies in its aspects of spirit, nature, being-ness, and of the cosmos. This trinity symbol, as most others, reflects core concepts such as: Mind, Body, Spirit. Afterlife, Underworld, Physical life. Tribe, Country, Deity (god/goddess). These are just a few trinities folded within this Celtic symbol meaning. There are many more. The differences lay in the intent of the symbol, and the tribe that was using at the time in Celtic history. The negative space in the center of this symbol symbolizes the center of all influences. In personal ritual, it could represent the self at the center of trinity influences. In a tribe, the center is the heart of the people with the three elements creating an unbreakable union."},
+    {name: "Triskele", origin: "Celtic", description: "This is another trinity symbol often found among ancient Celtic relics. It is sometimes referred to as a triskelion. The terms are often considered synonymous. This Celtic symbol meaning deals with the drawing of the three powers of maiden, mother and crone. It is often considered a sign of female power and especially power through transition and growth. This, however, is not an exclusive definition. Sometimes the symbol represents religious aspects of Father, Son, Holy Ghost. This representation came much later amongst Celtic descendants who were exposed to Christian conversion. In any instance, this trinity symbol can represent a triangle of foundational necessity. Meaning - you cannot have a healthy whole without all three elements joined together. Hence: Mind, Body, Spirit. Sun, Moon, Earth. Light, Dark, Gloaming (twilight). Not one energy can exist without the others."},
+    {name: "Awen", origin: "Celtic", description: "This is the three rays, or three flames symbol - also known as the Awen. The first and third rays in this symbol represent male and female energy (respectively). The middle ray represents the balance of both energies. At its heart, this symbol expresses the constant theme of the trinity. Threes are a massive influence amongst the Celtic people. Mostly, because it is clear there cannot be balance, or a whole unit without the union of three core energies. For example: Male, Female, Offspring. God, Goddess, (Hu)Man. Creating, Destroying, Sustaining. Past, Present, Future. The ancient Celts used the Awen as a symbol for transformation experienced through the power of fire. If the Celtic Awen symbol could speak, it might say: 'I was a certain entity. Fire burned and changed me. Now I am completely transformed.'"}
+  ])
+
+Trait.create! ([
+    {name: "symmetric"},
+    {name: "asymmetric"},
+    {name: "open"},
+    {name: "closed"},
+    {name: "open and closed"},
+    {name: "straight"},
+    {name: "curved"},
+    {name: "straight and curved"},
+    {name: "circle"},
+    {name: "rectangle"},
+    {name: "square"},
+    {name: "spiral"},
+    {name: "knot"},
+    {name: "star"},
+    {name: "cross"},
+    {name: "single"},
+    {name: "double"},
+    {name: "triple"}
+  ])
+
+SympolTrait.create!([
+  {sympol_id: 1, trait_id: 7},
+  {sympol_id: 1, trait_id: 4},
+  {sympol_id: 1, trait_id: 1},
+  {sympol_id: 1, trait_id: 13},
+  {sympol_id: 1, trait_id: 18},
 
 
+  {sympol_id: 2, trait_id: 2},
+  {sympol_id: 2, trait_id: 3},
+  {sympol_id: 2, trait_id: 7},
+  {sympol_id: 2, trait_id: 12},
+  {sympol_id: 2, trait_id: 3},
 
 
+  {sympol_id: 3, trait_id: 1},
+  {sympol_id: 3, trait_id: 4},
+  {sympol_id: 3, trait_id: 8},
+  {sympol_id: 3, trait_id: 9},
+  {sympol_id: 3, trait_id: 16},
+])
 
-
-
-
-
+Image.create!([
+    {url: "/images/Celtic/Triquetra.jpg", sympol_id: 1},
+    {url: "/images/Celtic/Triskele.jpg", sympol_id: 2},
+    {url: "/images/Celtic/Awen.jpg", sympol_id: 3}
+])
 
 
 
--- to_keep.rb
+++ to_keep.rb
@@ -1,3 +1,15 @@
+S3 KEYS:
+
+User name,Password,Access key ID,Secret access key,Console login link
+admin-sympol,,AKIAIBJ46QN2ZADBRD6A,LUB0Jtroj780UDehIkH7lMmH1h9yhx9ekKN6s4sh,https://763583573852.signin.aws.amazon.com/console
+
+"Principal": { "AWS": "d56cd60aed434e87bb5bb5ced08cd299a0b5b08e6bcca7e08544ef060513ea4d" }
+
+"Principal": { "AWS": "arn:aws:iam::763583573852:root" }
+
+arn:aws:s3:::sympol-app
+
+
 <!-- Josh explained sympol name index search -->
 <template id="sympols-index-page">
   <div class="vue-component">
