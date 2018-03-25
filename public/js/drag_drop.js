a0421829c0ed29cb3495a701821c303e29985e02
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-03-25 17:19:37 -0500

    improved styling

--- public/js/drag_drop.js
+++
@@ -1,9 +0,0 @@
-dropContainer.ondragover = dropContainer.ondragenter = function(evt) {
-  evt.preventDefault();
-};
-
-dropContainer.ondrop = function(evt) {
-  fileInput.files = evt.dataTransfer.files;
-  evt.preventDefault();
-};
-
