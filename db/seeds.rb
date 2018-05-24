7f85ef4bd1bfdd73bebcd832cb904d14c0f1f335
Ciaran <ciaran_mcgoldrick@hotmail.com>
2018-05-24 18:57:38 -0500

    yo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-        # PRODUCTION
+#         # PRODUCTION
 Sympol.create!([
     {name: "Triquetra", origin: "Celtic", description: "Latin meaning 'threecornered' It's a holy symbol, and it's meanings are many. It varies in its aspects of spirit, nature, being-ness, and of the cosmos. This trinity symbol, as most others, reflects core concepts such as: Mind, Body, Spirit. Afterlife, Underworld, Physical life. Tribe, Country, Deity (god/goddess). These are just a few trinities folded within this Celtic symbol meaning. There are many more. The differences lay in the intent of the symbol, and the tribe that was using at the time in Celtic history. The negative space in the center of this symbol symbolizes the center of all influences. In personal ritual, it could represent the self at the center of trinity influences. In a tribe, the center is the heart of the people with the three elements creating an unbreakable union."},
     {name: "Triskele", origin: "Celtic", description: "This is another trinity symbol often found among ancient Celtic relics. It is sometimes referred to as a triskelion. The terms are often considered synonymous. This Celtic symbol meaning deals with the drawing of the three powers of maiden, mother and crone. It is often considered a sign of female power and especially power through transition and growth. This, however, is not an exclusive definition. Sometimes the symbol represents religious aspects of Father, Son, Holy Ghost. This representation came much later amongst Celtic descendants who were exposed to Christian conversion. In any instance, this trinity symbol can represent a triangle of foundational necessity. Meaning - you cannot have a healthy whole without all three elements joined together. Hence: Mind, Body, Spirit. Sun, Moon, Earth. Light, Dark, Gloaming (twilight). Not one energy can exist without the others."},
@@ -352,12 +352,12 @@ Image.create!([
     {url: "/images/Zodiac/Virgo.png", sympol_id: 142},
     {url: "/images/Zodiac/Cancer.png", sympol_id: 143},
 
-{url: "images/Sak_Yant/hah_taew_.jpg", sympol_id: "144"},
-{url: "images/Sak_Yant/gao_yord_one.jpg", sympol_id: "145"},
-{url: "images/Sak_Yant/paed_tidt_one.jpg", sympol_id: "146"},
-{url: "images/Sak_Yant/hanuman_one.jpg", sympol_id: "147"},
-{url: "images/Sak_Yant/Yant_suea.jpg", sympol_id: "148"},
-{url: "images/Sak_Yant/chat_petch.jpg", sympol_id: "149"}
+{url: "images/Sak_Yant/hah_taew_.jpg", sympol_id: 144},
+{url: "images/Sak_Yant/gao_yord_one.jpg", sympol_id: 145},
+{url: "images/Sak_Yant/paed_tidt_one.jpg", sympol_id: 146},
+{url: "images/Sak_Yant/hanuman_one.jpg", sympol_id: 147},
+{url: "images/Sak_Yant/Yant_suea.jpg", sympol_id: 148},
+{url: "images/Sak_Yant/chat_petch.jpg", sympol_id: 149}
 
 
 
